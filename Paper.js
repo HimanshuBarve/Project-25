a1325b7c515db21de6eb19f40fcc57a65c1387f1
HimanshuBarve <74904340+HimanshuBarve@users.noreply.github.com>
2021-04-25 15:24:23 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,27 @@
+class Paper{
+	constructor(x,y,r)
+	{
+	// assign options to the rubber ball
+	var options={restitution:0.3,friction:0,density:1.3,isStatic:false}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+this.image=loadImage("paper.png")
+	}
+	display()
+	{
+			var paperpos=this.body.position;		
+			push()
+			translate(paperpos.x, paperpos.y);
+			imageMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("darkblue");
+			//draw the ellipse here to display the rubber ball
+image(this.image,0,0,this.r,this.r)
+			pop()
+	}
+
+}
